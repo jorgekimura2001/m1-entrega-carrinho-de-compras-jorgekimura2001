9948a9ea4f312345a3450bbafb8408b1d21280ab
Jorge Kimura <kimurajorge2001@gmail.com>
2022-05-10 22:29:03 -0300

    Arquivo JS

--- index.js
+++ index.js
@@ -1,64 +1,80 @@
-const body = document.querySelector('body')
+const body = document.querySelector("body");
 
-const productsCart = [ // variavel de array de objetos de produtos
-    {
-        id: 1,
-        nome: "desinfetante",
-        preco: 20.00,
-    },
-    {
-        id: 2,
-        nome: 'amaciante',
-        preco: 20.00 
-    },
-    {
-        id: 3,
-        nome: 'sabão em pó',
-        preco: 15.90
-    },
-    {
-        id: 4,
-        nome: 'detergente',
-        preco: 2.50
-    },
-    {
-        id: 5,
-        nome: 'biscoito',
-        preco: 3.50 
-    }
-]
+const productsCart = [
+  // variavel de array de objetos de produtos
+  {
+    id: 1,
+    nome: "desinfetante",
+    preco: 20.0,
+  },
+  {
+    id: 2,
+    nome: "amaciante",
+    preco: 20.0,
+  },
+  {
+    id: 3,
+    nome: "sabão em pó",
+    preco: 15.9,
+  },
+  {
+    id: 4,
+    nome: "detergente",
+    preco: 2.5,
+  },
+  {
+    id: 5,
+    nome: "biscoito",
+    preco: 3.5,
+  },
+];
 
-let soma = 0 // variavel para representar a soma total dos itens
+let soma = 0; // variavel para representar a soma total dos itens
 
 // const div = document.createElement('div')
 
 // div.className = 'produtosItem' --> para criar uma classe
 
-const main = document.createElement('main')
+const main = document.createElement("main"); // criando o elemento HTML MAIN
 
-const productsList = document.createElement('ul')
+const productsList = document.createElement("ul"); // CRIANDO O ELEMENTO UL (lista não ordenada)
 
-main.appendChild(productsList)
+main.appendChild(productsList); // main (pai) --> ul (filho)
 
-const valorFinal = document.createElement('span')
+const valorFinal = document.createElement("span"); // guardar o valor da soma em uma variavel
 
-main.appendChild(valorFinal)
+main.appendChild(valorFinal); // main --> ul e span
 
-function percorrerCompras (arrayProdutos){
-    for(let i = 0; i < arrayProdutos.length; i++){    // percorreu o array e criou li 
-        productsList.innerHTML += `<li>Nome: ${arrayProdutos[i].nome}</li> 
-        <li> Valor: ${arrayProdutos[i].preco} </li>`
-        soma += arrayProdutos[i].preco 
-        valorFinal.innerHTML = `${soma}`
-    }
+function percorrerCompras(arrayProdutos) {
+  // função para percorrer o array de objetos e criar li dentro da ul
+  for (let i = 0; i < arrayProdutos.length; i++) {
+    // += incrementar na li o nome e o preco
+    productsList.innerHTML += `<li>${arrayProdutos[i].nome}</li> 
+        <li>${arrayProdutos[i].preco} </li>`;
+    soma += arrayProdutos[i].preco; // como o preço já é número pode se fazer a soma de maneira 'comum'
+    valorFinal.innerHTML = `Total da sua compra é R$${soma}`; // span é igual ao valor da soma
+  }
 }
-percorrerCompras(productsCart)
+percorrerCompras(productsCart); // chamando a função, utilizando como argumento o nosso array de produtos
 
-body.appendChild(main)
+body.appendChild(main); // body --> main --> ul e span sendo ul pai de li
 
-const botao = document.createElement('button') // button
+const divBotao = document.createElement('div')
 
-botao.innerText = 'Finalizar' // string no botão
+divBotao.className = 'botao'
 
-document.body.appendChild(botao)
+const botao = document.createElement("button"); // button
 
+botao.innerText = "Finalizar"; // string no botão
+
+const divSoma = document.createElement('div')
+
+divSoma.className = 'valordasoma'
+
+divSoma.appendChild(valorFinal)
+
+divBotao.appendChild(botao);
+
+body.appendChild(divSoma)
+
+body.appendChild(divBotao)
